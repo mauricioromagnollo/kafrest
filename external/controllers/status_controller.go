c129596c5cd49dc4059e1a609284634cccce29e5
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-10-25 22:50:54 -0300

    feat: change /status response

--- external/controllers/status_controller.go
+++ external/controllers/status_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/mauricioromagnollo/kafrest/external/config"
@@ -10,13 +11,30 @@ type StatusController struct {
 	Environment *config.Environment
 }
 
+type StatusResponse struct {
+	Message string `json:"message"`
+	Broker  string `json:"broker"`
+}
+
 func NewStatusController(env *config.Environment) *StatusController {
 	return &StatusController{
 		Environment: env,
 	}
 }
 
-func (sc StatusController) Handle(w http.ResponseWriter, r *http.Request) {
+func (sc StatusController) Handle(w http.ResponseWriter, _ *http.Request) {
+	responseData := StatusResponse{
+		Message: "OK",
+		Broker:  sc.Environment.KafkaHost,
+	}
+
+	responseJSON, err := json.Marshal(responseData)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Published successfully"))
+	w.Write(responseJSON)
 }
