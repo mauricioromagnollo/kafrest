6457d64b21fb83e68ecfc79895afdd9ac13a0783
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-10-27 00:35:29 -0300

    feat: add publish topic messages resources

--- external/controllers/publish_topic_messages.go
+++ external/controllers/publish_topic_messages.go
@@ -5,43 +5,48 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/mauricioromagnollo/kafrest/external/config"
 	"github.com/segmentio/kafka-go"
 )
 
-// PublishController is the struct that will handle the /publish endpoint.
-type PublishController struct {
+// PublishTopicMessagesController is the struct that will handle the /topics/{topicName}/messages endpoint.
+type PublishTopicMessagesController struct {
 	Environment *config.Environment
+	Logger      *config.Logger
 }
 
-type publishRequest struct {
-	Topic    string                   `json:"topic"`
+type publishTopicsRequest struct {
 	Messages []map[string]interface{} `json:"messages"`
 }
 
-type publishResponse struct {
-	MessagesPublished []map[string]interface{} `json:"messages_published"`
+type publishTopicsResponse struct {
+	Topic             string                   `json:"topic"`
+	PublishedMessages []map[string]interface{} `json:"published_messages"`
 }
 
-// NewPublishController is the constructor for the PublishController struct.
-func NewPublishController(env *config.Environment) *PublishController {
-	return &PublishController{
+// NewPublishTopicMessagesController is the constructor for the PublishTopicMessages struct.
+func NewPublishTopicMessagesController(env *config.Environment, logger *config.Logger) *PublishTopicMessagesController {
+	return &PublishTopicMessagesController{
 		Environment: env,
+		Logger:      logger,
 	}
 }
 
 // Handle is the function that will be called when a request is made to the /publish endpoint.
-func (pc PublishController) Handle(w http.ResponseWriter, r *http.Request) {
-	var requestData publishRequest
-	var responseData publishResponse
+func (pc PublishTopicMessagesController) Handle(w http.ResponseWriter, r *http.Request) {
+	var requestData publishTopicsRequest
+	var responseData publishTopicsResponse
 
 	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
 		return
 	}
 
+	topicName := chi.URLParam(r, "topicName")
+
 	kw := kafka.Writer{
 		Addr:     kafka.TCP(pc.Environment.KafkaHost),
-		Topic:    requestData.Topic,
+		Topic:    topicName,
 		Balancer: &kafka.LeastBytes{},
 	}
 
@@ -67,9 +72,10 @@ func (pc PublishController) Handle(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		responseData.MessagesPublished = append(responseData.MessagesPublished, message)
+		responseData.PublishedMessages = append(responseData.PublishedMessages, message)
 	}
 
+	responseData.Topic = topicName
 	responseJSON, err := json.Marshal(responseData)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
